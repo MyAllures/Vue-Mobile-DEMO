042043908b15db9b441db02033e441aba412917c
Snow <snow@unnotech.com>
2017-12-19 16:39:10 +0800

    added my page and fin page structure

--- src/i18n/cn.js
+++ src/i18n/cn.js
@@ -1,6 +1,20 @@
 'use strict'
 
 export default {
+  misc: {
+    username: '用户名',
+    password: '密码',
+    login: '登录',
+    register: '注册',
+    try: '试玩',
+    bank: '取款账号',
+    phone: '手机号码',
+    email: '邮箱',
+    qq: 'QQ',
+    wechat: '微信',
+    reset_password: '重置密码',
+    reset_withdraw_password: '重置取款密码'
+  },
   home: {
     name: '首页',
     announcement: '公告'
@@ -12,14 +26,11 @@ export default {
     name: '财务'
   },
   my: {
-    name: '我的'
+    name: '我的',
+    balance: '余额',
+    message: '未读消息'
   },
   login: {
-    welcome: '欢迎登录',
-    username: '用户名',
-    password: '密码',
-    login: '登录',
-    register: '注册',
-    try: '试玩'
+    welcome: '欢迎登录'
   }
 }
--- src/screens/My.vue
+++ src/screens/My.vue
@@ -1,18 +1,107 @@
 <template>
   <div>
-    My
+    <group>
+      <cell :title="$t('my.balance')" >
+        <i class="icon-sum icon" slot="icon"></i>
+        <span class="balance text-green">￥{{user.balance.toFixed(1)}}</span>
+      </cell>
+      <cell :title="$t('misc.bank')" :is-link="true">
+        <i class="icon-bank icon" slot="icon"></i>
+        <span>{{bankAccount}}</span>
+      </cell>
+    </group>
+    <group>
+      <cell 
+        :title="$t('my.message')"
+        :is-link="true">
+        <i class="icon-msg icon" slot="icon"></i>
+        <span>x 未读</span>
+      </cell>
+      <cell 
+        :title="$t('misc.username')">
+        <i class="icon-user icon" slot="icon"></i>
+        <span>{{user.username}}</span>
+      </cell>
+    </group>
+    <group>
+      <cell 
+        :title="$t('misc.phone')">
+        <i class="icon-phone icon" slot="icon"></i>
+        <span>{{user.phone || '未填写'}}</span>
+      </cell>
+      <cell 
+        :title="$t('misc.email')"
+        :is-link="true">
+        <i class="icon-email icon" slot="icon"></i>
+        <span>{{user.email || '未填写'}}</span>
+      </cell>
+      <cell 
+        :title="$t('misc.qq')"
+        :is-link="true">
+        <i class="icon-QQ icon" slot="icon"></i>
+        <span>{{user.qq || '未填写'}}</span>
+      </cell>
+      <cell 
+        :title="$t('misc.wechat')"
+        :is-link="true">
+        <i class="icon-wechat icon" slot="icon"></i>
+        <span>{{user.wechat || '未填写'}}</span>
+      </cell>
+    </group>
+
+    <group>
+      <cell 
+        :title="$t('misc.reset_password')"
+        :is-link="true">
+        <i class="icon-password icon" slot="icon"></i>
+      </cell>
+      <cell 
+        :title="$t('misc.reset_withdraw_password')"
+        :is-link="true">
+        <i class="icon-pass icon" slot="icon"></i>
+      </cell>
+    </group>
   </div>
 </template>
 
 <script>
+import '../styles/fonts/icons.css'
+import { mapGetters } from 'vuex'
+import { Group, Cell } from 'vux'
+
 export default {
   name: 'Home',
   data () {
     return {
     }
+  },
+  computed: {
+    ...mapGetters([
+      'user'
+    ]),
+    bankAccount () {
+      let bank = this.user.bank
+      if (bank) {
+        let bankName = this.user.bank.bank
+        let bankNo = this.user.bank.account.slice(-4)
+        return `${bankName} ****${bankNo}`
+      }
+      return ''
+    }
+  },
+  components: {
+    Group,
+    Cell
   }
 }
 </script>
 
-<style scoped>
+<style scoped >
+.icon {
+  width: 24px;
+  margin-right: 10px;
+  font-size: 20px;
+  display: inline-block;
+  vertical-align: middle;
+}
 </style>
