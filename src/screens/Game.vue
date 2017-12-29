b61d7cf30affef5a719e63ed2674f9a6a4938171
sunday0127 <sunday@unnotech.com>
2017-12-29 17:07:38 +0800

    reset

--- src/screens/Game.vue
+++ src/screens/Game.vue
@@ -1,45 +1,18 @@
 <template>
   <div>
-    <button-tab class="m-r-l m-l-l m-t m-b">
-      <button-tab-item selected @on-item-click="initDeposit">{{ $t('deposit.title') }}</button-tab-item>
-      <button-tab-item @on-item-click="initRemit">{{ $t('deposit.remit') }}</button-tab-item>
-    </button-tab>
-    <div class="divide"></div>
-    <transition  name="component-fade" mode="out-in">
-      <component v-bind:is="view"></component>
-    </transition>
+    Game
   </div>
 </template>
 
 <script>
-  import { ButtonTab, ButtonTabItem } from 'vux'
-  import remit from './remit.vue'
-  import deposit from './deposit.vue'
   export default {
+    name: 'Home',
     data () {
       return {
-        view: deposit
-      }
-    },
-    components: {
-      ButtonTab,
-      ButtonTabItem
-    },
-    methods: {
-      initDeposit () {
-        this.view = deposit
-      },
-      initRemit () {
-        this.view = remit
       }
     }
   }
 </script>
 
-<style>
-  .divide {
-    width: 100%;
-    height: 1px;
-    background: #ccc;
-  }
+<style scoped>
 </style>
