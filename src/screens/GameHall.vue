ca1c577dc729402b7a0bd9d0c270aaa2212345d2
sunday0127 <sunday@unnotech.com>
2018-01-23 11:56:04 +0800

    fix bug

--- src/screens/GameHall.vue
+++ src/screens/GameHall.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="clear-viewbox-default-top clear-viewbox-default-bottom gamehall">
     <!-- 头部 -->
-    <x-header class="gamehall" :right-options="{showMore: true}" @on-click-more="showRightMenu = true">
+    <x-header class="gamehall-header" :right-options="{showMore: true}" @on-click-more="showRightMenu = true">
       <x-icon
         slot="overwrite-left"
         type="navicon"
