fff5462084dbd8428b5db8ff9d3ec46c23b1b965
kayne@unnotech.com <kayne@unnotech.com>
2018-01-26 16:51:36 +0800

    fix fetchUser time to apply requirement

--- src/screens/games/Game.vue
+++ src/screens/games/Game.vue
@@ -290,6 +290,7 @@ export default {
             this.showMessage = true
             this.dialogVisible = false
             this.loading = false
+            this.$store.dispatch('fetchUser')
           } else {
             let messages = []
             res.msg.forEach(error => {
