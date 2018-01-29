e9b91d998f51b86b921540bca13c4a32ae537fd9
jerrywei <jerrywei17@unnotech.com>
2018-01-29 11:39:03 +0800

    Add fc3d new play

---
+++ src/components/playGroup/fc3dIc.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="gameplays">
+    <div class="playgroup-title">{{groupName}}</div>
+    <div class="playgroup-odds">赔率：{{activePlay.odds}}</div>
+    <grid :cols="2">
+      <grid-item
+        :class="['play', {active: option.active && !gameClosed}]"
+        v-for="(option, index) in customOptions"
+        :key="index"
+        @on-item-click="toggleActive(option)">
+        <div class="play-area">
+          <span class="play-name">{{option.num}}</span>
+        </div>
+      </grid-item>
+    </grid>
+  </div>
+</template>
+<script>
+import _ from 'lodash'
+import { Grid, GridItem } from 'vux'
+export default {
+  name: 'fc3dIc',
+  props: {
+    plays: {
+      type: Array
+    },
+    gameClosed: {
+      type: Boolean
+    },
+    playReset: {
+      type: Boolean
+    },
+    setting: {
+      type: Object
+    },
+    groupName: {
+      type: String
+    }
+  },
+  components: {
+    Grid,
+    GridItem
+  },
+  data () {
+    const customOptions = []
+    this.setting.options.forEach(option => {
+      customOptions.push({
+        num: option,
+        active: false
+      })
+    })
+    return {
+      customOptions,
+      combinations: [],
+      valid: false,
+      activePlay: {
+        id: '',
+        display_name: '',
+        odds: '--'
+      }
+    }
+  },
+  computed: {
+    activedOptions () {
+      return _.filter(this.customOptions, option => {
+        return option.active
+      })
+    },
+    maxOptionCount () {
+      return (this.plays.length - 1) * 2
+    }
+  },
+  watch: {
+    'activedOptions': function () {
+      if (this.activedOptions.length < this.maxOptionCount / 2) {
+        this.updateForSubmit()
+        this.activePlay.odds = '--'
+        this.activePlay.id = ''
+        this.activePlay.display_name = ''
+        return
+      }
+
+      this.activedOptions.sort((a, b) => { return a.num - b.num })
+      _.forEach(this.plays, (play) => {
+        if (play.display_name.slice(-1) === '' + (this.activedOptions.length)) {
+          this.activePlay.odds = play.odds
+          this.activePlay.id = play.id
+          this.activePlay.display_name = play.display_name
+          this.updateForSubmit()
+        }
+      })
+    },
+    'playReset': function () {
+      this.customOptions.forEach(option => {
+        option.active = false
+      })
+      this.combinations.length = 0
+    }
+  },
+  methods: {
+    updateForSubmit () {
+      this.valid = this.activedOptions.length >= this.maxOptionCount / 2
+      this.$emit('updateCustomPlays', {
+        activePlayId: this.activePlay.id,
+        activedOptions: this.activedOptions.map(option => {
+          return {
+            num: option.num
+          }
+        }),
+        combinations: '',
+        valid: this.valid
+      })
+    },
+    toggleActive (option) {
+      if (this.gameClosed) {
+        return false
+      }
+      if (!option.active) {
+        if (this.activedOptions.length < this.maxOptionCount) {
+          option.active = true
+        }
+      } else {
+        option.active = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "../../styles/vars.less";
+@import "../../styles/playgroup.less";
+.odds {
+  width: 100%;
+  line-height: 40px;
+  height: 40px;
+  text-align: center;
+  font-size: 14px;
+}
+</style>
