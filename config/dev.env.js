538b48b208d96ae725f1f7ed336a526d73fe36e5
jerrywei <jerrywei17@unnotech.com>
2019-02-12 11:29:59 +0800

    feat(*): right menu ui polish

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,9 +3,10 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  HOST: '"https://api.h9339.com"',
+  // HOST: '"http://staging-api.h9339.com"',
+  HOST: '"/api"',
   chatHost: '"ws://b583747.eastasia.cloudapp.azure.com:8003"',
   chatApi: '"http://b583747.eastasia.cloudapp.azure.com:8003"',
-  eiderHost: '"wss://ws.h9339.com:8445"',
+  eiderHost: '"ws://staging-eider.h9339.com"',
   company: '"staging"'
 })
