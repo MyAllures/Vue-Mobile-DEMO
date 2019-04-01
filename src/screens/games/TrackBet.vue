0a755c25a22125baecf9d5ae98204000cf2be7b3
Lay <lay@unnotech.com>
2019-04-01 09:25:37 +0800

    feat(*): add ynssc

--- src/screens/games/TrackBet.vue
+++ src/screens/games/TrackBet.vue
@@ -80,6 +80,10 @@ const trackOptions = {
     tracks: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     positionNum: 5
   },
+  ynssc: {
+    tracks: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    positionNum: 5
+  },
   jsssc: {
     tracks: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     positionNum: 5
--- src/utils/gameTranslator.js
+++ src/utils/gameTranslator.js
@@ -264,6 +264,7 @@ export default {
   'cs600cr': transformerTranslator,
   'cqssc': ssTranslator,
   'jsssc': ssTranslator,
+  'ynssc': ssTranslator,
   'csffc': ssTranslator,
   'xjssc': ssTranslator,
   'tjssc': ssTranslator,
