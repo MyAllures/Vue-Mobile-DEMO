a1226d9129980c83214b5985e17e7b7282a01be2
Snow <snow@unnotech.com>
2019-04-12 11:07:19 +0800

    1. Add more tag on homepage 2. change game menu with in category component 3. other ui updates

--- src/components/WinHistory.vue
+++ src/components/WinHistory.vue
@@ -33,7 +33,17 @@ export default {
       bottomItems: [],
       index: 0,
       timeout: null,
+      rollingInterval: setInterval(this.rolling, 50),
       filterHKL: true
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      if (to.name === 'Home') {
+        this.rollingInterval = setInterval(this.rolling, 50)
+      } else {
+        clearInterval(this.rollingInterval)
+      }
     }
   },
   created () {
@@ -42,11 +52,10 @@ export default {
     let hklEnd = this.$moment('9:45pm', 'h:mma')
     this.filterHKL = ![2, 4, 6].includes(now.day()) || now.isBefore(hklBegain) || now.isAfter(hklEnd)
 
-    this.fillWinHistory().then(() => {
-      this.$nextTick(() => {
-        setInterval(this.rolling, 50)
-      })
-    })
+    this.fillWinHistory()
+  },
+  beforeDestroy () {
+    clearInterval(this.rollingInterval)
   },
   methods: {
     refreshItems () {
@@ -60,7 +69,6 @@ export default {
         if (this.filterHKL) {
           res = res.map(item => {
             if (item.display === '香港六合彩') {
-              console.log('found')
               item.display = '极速六合彩'
             }
             return item
@@ -82,7 +90,10 @@ export default {
     },
     rolling () {
       let wrapper = this.$refs.wrapper
-      let firstList = wrapper.children && wrapper.children[0]
+      if (!wrapper || !wrapper.children) {
+        return
+      }
+      let firstList = wrapper.children[0]
       if (wrapper.scrollTop >= firstList.scrollHeight) {
         wrapper.scrollTop = 0
         // 滚动到顶部的时候 两个 ul 换位
