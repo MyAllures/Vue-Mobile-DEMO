2d7a0c7ef4980dce3cbe559ac66b642027239969
kayne@unnotech.com <kayne@unnotech.com>
2019-05-07 01:45:24 +0800

    feat(*): change comparision category in Lotter Record

---
+++ src/utils/lotteryRecordSetting.js
@@ -0,0 +1,19 @@
+
+const changeableCompareTypes = {
+  1: {
+    types: ['number', 'thanSize', 'oddEven'],
+    games: ['jspk10', 'bcr', 'mlaft', 'er75ft', 'cs60cr', 'cs600cr', 'cqssc', 'jsssc', 'xjssc', 'tjssc', 'csffc', 'cs10fc', 'cs5fc', 'ynssc', 'hjssc']
+  },
+  2: {
+    types: ['number', 'zodiac', 'thanSize', 'oddEven', 'ballOfSumThanSize', 'ballOfSumOddEven', 'tailThanSize'],
+    games: ['hkl', 'luckl', 'cshkl', 'cs600hkl', 'csjndhkl', 'cs75hkl']
+  },
+  3: {
+    types: ['number', 'thanSize', 'oddEven', 'ballOfSumOddEven', 'tailThanSize'],
+    games: ['cqlf', 'gdklsf']
+  }
+}
+
+export {
+  changeableCompareTypes
+}
