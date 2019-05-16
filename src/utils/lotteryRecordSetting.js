fb1fb80ae20e8666448f5bdf4e38a5054366bcae
kasey <kasey@unnotech.com>
2019-05-16 14:36:40 +0800

    fix(*)change fh11x5 --> fj11x5

--- src/utils/lotteryRecordSetting.js
+++ src/utils/lotteryRecordSetting.js
@@ -2,7 +2,7 @@
 const changeableCompareTypes = {
   1: {
     types: ['number', 'thanSize', 'oddEven'],
-    games: ['jspk10', 'bcr', 'mlaft', 'er75ft', 'cs60cr', 'cs600cr', 'cqssc', 'jsssc', 'xjssc', 'tjssc', 'csffc', 'cs10fc', 'cs5fc', 'ynssc', 'hjssc', 'gd11x5', 'ah11x5', 'bj11x5', 'fh11x5', 'gs11x5']
+    games: ['jspk10', 'bcr', 'mlaft', 'er75ft', 'cs60cr', 'cs600cr', 'cqssc', 'jsssc', 'xjssc', 'tjssc', 'csffc', 'cs10fc', 'cs5fc', 'ynssc', 'hjssc', 'gd11x5', 'ah11x5', 'bj11x5', 'fj11x5', 'gs11x5']
   },
   2: {
     types: ['number', 'zodiac', 'thanSize', 'oddEven', 'ballOfSumThanSize', 'ballOfSumOddEven', 'tailThanSize'],
