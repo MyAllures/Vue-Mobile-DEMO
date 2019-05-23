48e0790f1e9f8b978dd21f2bac17c26b9a5766ca
jerrywei <jerrywei17@unnotech.com>
2019-05-23 15:43:37 +0800

    fix(chatroom.vue): chatroom should be created after getting token

--- src/store/token/index.js
+++ src/store/token/index.js
@@ -7,11 +7,19 @@ export default {
   mutations: {
     addToken: (state, {type, token}) => {
       state[type] = token
+    },
+    clear: (state) => {
+      Object.keys(state).forEach(key => {
+        state[key] = ''
+      })
     }
   },
   actions: {
     addToken: ({ commit }, data) => {
       commit('addToken', data)
+    },
+    clear: ({ commit }) => {
+      commit('clear')
     }
   }
 }
